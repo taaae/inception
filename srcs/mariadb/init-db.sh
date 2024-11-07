37dab6143659fe1861010c46b60a95a9c2028e64
timkingteam <timkingteam@gmail.com>
2024-11-07 13:23:07 +0100

    dockerfile mariadb: finally works

---
+++ srcs/mariadb/init-db.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+service mariadb start
+
+cat << EOF | mariadb
+CREATE DATABASE ${MARIADB_DATABASE};
+CREATE USER '${MARIADB_USER}' IDENTIFIED BY '$(cat $MARIADB_PASSWORD_FILE)';
+GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO '${MARIADB_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+cat << EOF >> /etc/mysql/my.cnf
+
+[mysqld]
+bind-address = 0.0.0.0
+EOF
+
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '$(cat $MARIADB_PASSWORD_FILE)';
+
+mysqladmin -u root -p"$(cat $MARIADB_ROOT_PASSWORD_FILE)" shutdown
+
+exec "$@"
