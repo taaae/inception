9ced7da57f48fe25f26274bf56fc3e3f344d5bc8
timkingteam <timkingteam@gmail.com>
2024-11-09 14:55:26 +0100

    mariadb: do not run database code the second time

--- srcs/mariadb/init-db.sh
+++ srcs/mariadb/init-db.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -e
 
-service mariadb start
+if [ ! -f /db_setup_done ]; then
+    service mariadb start
 
-cat << EOF | mariadb
+    cat << EOF | mariadb
 CREATE DATABASE ${MARIADB_DATABASE};
 CREATE USER '${MARIADB_USER}' IDENTIFIED BY '$(cat $MARIADB_PASSWORD_FILE)';
 GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO '${MARIADB_USER}'@'%';
@@ -11,12 +12,17 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY '$(cat $MARIADB_ROOT_PASSWORD_FILE)'
 FLUSH PRIVILEGES;
 EOF
 
-cat << EOF >> /etc/mysql/my.cnf
+    cat << EOF >> /etc/mysql/my.cnf
 
 [mysqld]
 bind-address = 0.0.0.0
 EOF
 
-mysqladmin -u root -p"$(cat $MARIADB_ROOT_PASSWORD_FILE)" shutdown
+    mysqladmin -u root -p"$(cat $MARIADB_ROOT_PASSWORD_FILE)" shutdown
+
+    touch /db_setup_done # flag file to indicate setup is complete
+else
+    echo "Skipping one-time MariaDB setup, already completed."
+fi
 
 exec "$@"
