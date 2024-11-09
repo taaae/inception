7c64de89d8aac84335b45375436f7e41d9153dda
timkingteam <timkingteam@gmail.com>
2024-11-09 15:05:50 +0100

    wordpress user

--- srcs/wordpress/setup-wordpress.sh
+++ srcs/wordpress/setup-wordpress.sh
@@ -9,6 +9,7 @@ if [ ! -f /var/www/html/wp-config.php ]; then
     wp core download --allow-root
     wp config create --allow-root --dbuser=$WORDPRESS_DB_USER --dbhost=$WORDPRESS_DB_HOST --dbname=$WORDPRESS_DB_NAME --dbpass=$(cat $WORDPRESS_DB_PASSWORD_FILE)
     wp core install --allow-root --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_email=$WORDPRESS_ADMIN_EMAIL --admin_password=$(cat $WORDPRESS_ADMIN_PASSWORD_FILE)
+    wp user create --allow-root $WORDPRESS_USER_LOGIN $WORDPRESS_USER_EMAIL --user_pass=$(cat $WORDPRESS_USER_PASSWORD_FILE)
 fi
 
 exec "$@"
